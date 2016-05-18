40c85e3dc5b22c7e277400a045c59b2a4fb22ef7
Trip-to-beaverland <aleksandr.menshchikov@skolkovotech.ru>
2016-05-19 00:17:32 +0400

    Add files via upload

--- Testing/GUI_Big_Robot.py
+++ Testing/GUI_Big_Robot.py
@@ -36,7 +36,9 @@ class GUI(object):
         frame=Frame(self.root)
         frame.grid(row=0,column=1, sticky=W)
 
-            ###ROBOT-COORDINATES###SMALL
+        ###SMALL ROBOT###
+        ###SMALL ROBOT###
+        ###ROBOT-COORDINATES###SMALL
 
         RobotCoordinates = Label(frame, text = "SMALL ROBOT")
         RobotCoordinates.grid(row = 1, column = 1)
@@ -93,12 +95,69 @@ class GUI(object):
         self.collValue = Text(frame, height=1, width=3)
         self.collValue.grid(row=14,column=2,sticky=W)
 
-            ###ADC###
+            ###SMALL ROBOT IR Range Finder###4
 
-        ADC = Label(frame, text="Client Small: ")
-        ADC.grid(row = 15, column = 1)
-        self.ADCValue = Text(frame, height=10, width=10)
-        self.ADCValue.grid(row=16, column=1)
+        IRrangeFinderS = Label(frame, text="IR Range Finder")
+        IRrangeFinderS.grid(row=15, column=1)
+
+        IR1S = Label(frame, text="IR - Right =")
+        IR1S.grid(row=16, column=1, sticky=E)
+
+        IR2S = Label(frame, text="IR - Left =")
+        IR2S.grid(row=17, column=1, sticky=E)
+
+        IR3S = Label(frame, text="IR - Front =")
+        IR3S.grid(row=18, column=1, sticky=E)
+
+        IR4S = Label(frame, text="IR - Back =")
+        IR4S.grid(row=19, column=1, sticky=E)
+
+        self.IR1SValue = Text(frame, height = 1, width = 5)
+        self.IR1SValue.grid(row = 16, column = 2)
+
+        self.IR2SValue = Text(frame, height = 1, width = 5)
+        self.IR2SValue.grid(row = 17, column = 2)
+
+        self.IR3SValue = Text(frame, height = 1, width = 5)
+        self.IR3SValue.grid(row = 18, column = 2)
+
+        self.IR4SValue = Text(frame, height = 1, width = 5)
+        self.IR4SValue.grid(row = 19, column = 2)
+
+        ###SMALL ROBOT ULTRASONIC RANGE FINDER###5
+
+        USrangeFinderS = Label(frame, text="Ultrasonic Range Finder")
+        USrangeFinderS.grid(row=20, column=1)
+
+        US1S = Label(frame, text="US - Right =")
+        US1S.grid(row=21, column=1, sticky=E)
+
+        US2S = Label(frame, text="US - Left =")
+        US2S.grid(row=22, column=1, sticky=E)
+
+        US3S = Label(frame, text="US - Front =")
+        US3S.grid(row=23, column=1, sticky=E)
+
+        US4S = Label(frame, text="US - Back-1 =")
+        US4S.grid(row=24, column=1, sticky=E)
+
+        US5S = Label(frame, text="US - Back-2 =")
+        US5S.grid(row=25, column=1, sticky=E)
+
+        self.US1SValue = Text(frame, height = 1, width = 5)
+        self.US1SValue.grid(row = 21, column = 2)
+
+        self.US2SValue = Text(frame, height = 1, width = 5)
+        self.US2SValue.grid(row = 22, column = 2)
+
+        self.US3SValue = Text(frame, height = 1, width = 5)
+        self.US3SValue.grid(row = 23, column = 2)
+
+        self.US4SValue = Text(frame, height = 1, width = 5)
+        self.US4SValue.grid(row = 24, column = 2)
+
+        self.US5SValue = Text(frame, height = 1, width = 5)
+        self.US5SValue.grid(row = 25, column = 2)
 
         ###BIG ROBOT########
         ###BIG ROBOT###
@@ -159,12 +218,74 @@ class GUI(object):
         self.collBValue = Text(frame, height=1, width=3)
         self.collBValue.grid(row=14,column=4,sticky=W)
 
-            ###BIG ROBOT ADC###
+            ###BIG ROBOT IR Range Finder###4
 
-        ADCB = Label(frame, text="Client Big: ")
+        IRrangeFinderB = Label(frame, text="IR Range Finder")
+        IRrangeFinderB.grid(row=15, column=3)
+
+        IR1B = Label(frame, text="IR - Right =")
+        IR1B.grid(row=16, column=3, sticky=E)
+
+        IR2B = Label(frame, text="IR - Left =")
+        IR2B.grid(row=17, column=3, sticky=E)
+
+        IR3B = Label(frame, text="IR - Front =")
+        IR3B.grid(row=18, column=3, sticky=E)
+
+        IR4B = Label(frame, text="IR - Back =")
+        IR4B.grid(row=19, column=3, sticky=E)
+
+        self.IR1BValue = Text(frame, height = 1, width = 5)
+        self.IR1BValue.grid(row = 16, column = 4)
+
+        self.IR2BValue = Text(frame, height = 1, width = 5)
+        self.IR2BValue.grid(row = 17, column = 4)
+
+        self.IR3BValue = Text(frame, height = 1, width = 5)
+        self.IR3BValue.grid(row = 18, column = 4)
+
+        self.IR4BValue = Text(frame, height = 1, width = 5)
+        self.IR4BValue.grid(row = 19, column = 4)
+
+        ###BIG ROBOT ULTRASONIC RANGE FINDER###5
+
+        USrangeFinderB = Label(frame, text="Ultrasonic Range Finder")
+        USrangeFinderB.grid(row=20, column=3)
+
+        US1B = Label(frame, text="US - Right =")
+        US1B.grid(row=21, column=3, sticky=E)
+
+        US2B = Label(frame, text="US - Left =")
+        US2B.grid(row=22, column=3, sticky=E)
+
+        US3B = Label(frame, text="US - Front =")
+        US3B.grid(row=23, column=3, sticky=E)
+
+        US4B = Label(frame, text="US - Back-1 =")
+        US4B.grid(row=24, column=3, sticky=E)
+
+        US5B = Label(frame, text="US - Back-2 =")
+        US5B.grid(row=25, column=3, sticky=E)
+
+        self.US1BValue = Text(frame, height = 1, width = 5)
+        self.US1BValue.grid(row = 21, column = 4)
+
+        self.US2BValue = Text(frame, height = 1, width = 5)
+        self.US2BValue.grid(row = 22, column = 4)
+
+        self.US3BValue = Text(frame, height = 1, width = 5)
+        self.US3BValue.grid(row = 23, column = 4)
+
+        self.US4BValue = Text(frame, height = 1, width = 5)
+        self.US4BValue.grid(row = 24, column = 4)
+
+        self.US5BValue = Text(frame, height = 1, width = 5)
+        self.US5BValue.grid(row = 25, column = 4)
+
+        """ADCB = Label(frame, text="Client Big: ")
         ADCB.grid(row = 15, column = 3)
         self.ADCBValue = Text(frame, height=10, width=10)
-        self.ADCBValue.grid(row=16, column=3)
+        self.ADCBValue.grid(row=16, column=3)"""
 
 def draw_small(X0,Y0,alpha):
     r = 63 #radius of circumscribed circle
@@ -209,11 +330,17 @@ if __name__ == '__main__':
     server.start()
     root = Tk()
     app = GUI(root)
+    file = open("Big_robot_log.txt", "w")
     while True:
         data = data_queue.get()####
         data1 = str(data)
         result = re.findall(r'[+-]?\d+(?:\.\d+)?', data1)
         print data
+
+        log_result = str(data)
+        file.write(log_result)#unsorted
+        file.write("\n")
+        
         #data = [0,1,2,3,4,5,6]###from shared memory
         #result = [0,1,2,3,4,5,6]
 
@@ -238,8 +365,32 @@ if __name__ == '__main__':
         app.collBValue.delete(1.0,2.0)
         app.collBValue.insert(tk.END, result[6])
 
-        app.ADCBValue.delete(1.0, 2.0)
-        app.ADCBValue.insert(tk.END, data)
+        app.IR1BValue.delete(1.0,2.0)
+        app.IR1BValue.insert(tk.END, result[7])
+
+        app.IR2BValue.delete(1.0,2.0)
+        app.IR2BValue.insert(tk.END, result[8])
+
+        app.IR3BValue.delete(1.0,2.0)
+        app.IR3BValue.insert(tk.END, result[9])
+
+        app.IR4BValue.delete(1.0,2.0)
+        app.IR4BValue.insert(tk.END, result[10])
+
+        app.US1BValue.delete(1.0,2.0)
+        app.US1BValue.insert(tk.END, result[11])
+
+        app.US2BValue.delete(1.0,2.0)
+        app.US2BValue.insert(tk.END, result[12])
+
+        app.US3BValue.delete(1.0,2.0)
+        app.US3BValue.insert(tk.END, result[13])
+
+        app.US4BValue.delete(1.0,2.0)
+        app.US4BValue.insert(tk.END, result[14])
+
+        app.US5BValue.delete(1.0,2.0)
+        app.US5BValue.insert(tk.END, result[15])
 
         a=float(result[0])###LIDAR OF BIG RoBOT
         b=float(result[1])
@@ -261,3 +412,4 @@ if __name__ == '__main__':
         app.canvas.delete(big_robot_lidar_line)
     root.geometry("1500x846")
     root.mainloop()
+    file.close()
